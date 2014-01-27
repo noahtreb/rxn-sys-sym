aafa21b119bfe716aaf7d8ef006df936c7a4edb7
noahtreb <noahtreb@b9da0bf2-66db-0f8a-60db-2360e31b104e>
2014-01-27 03:35:53 +0000

    Updated Distribution class.

--- src/Distribution.h
+++ src/Distribution.h
@@ -1,32 +1,40 @@
 #ifndef DISTRIBUTION_H
 #define DISTRIBUTION_H
 
-class Node {
+#include <random>
+
+class DistNode {
 public:
     double state;
     int count;
+    int cumCount;
     
-    Node(double state, int count);
-    virtual ~Node();
+    DistNode(double state, int count, int cumCount);
+    virtual ~DistNode();
 private:
 };
 
 class Distribution {
 public:
-    Distribution(int maxNodes);
+    Distribution(int maxNodes, int seed);
     virtual ~Distribution();   
     
     void addNode(double state, int count);    
     void clear();
     
-    void update(double* states, int length);
+    void update(double* statesArr, int length);
     
     int getNumNodes() const;
     double getState(int nodeId) const;
     int getCount(int nodeId) const;
+    
+    double calcDistance(Distribution* other) const;
+    double sample() const;
 private:
     int numNodes;
     int maxNodes;
-    Node** nodes;
+    DistNode** nodes;
+    
+    std::mt19937* rng;
 };
 #endif
