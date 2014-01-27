56a8a6e0f1574a1912933635060851f20545101b
noahtreb <noahtreb@b9da0bf2-66db-0f8a-60db-2360e31b104e>
2014-01-27 03:35:53 +0000

    Updated Distribution class.

--- trunk/src/Distribution.cpp
+++ trunk/src/Distribution.cpp
@@ -1,26 +1,32 @@
 #include "Distribution.h"
 #include <algorithm>
+#include <math.h>
+#include <random>
+#include <vector>
 
 using namespace std;
 
-Node::Node(double state, int count) {
+DistNode::DistNode(double state, int count, int cumCount) {
     this->state = state;
     this->count = count;
+    this->cumCount = cumCount;
 }
 
-Node::~Node() {
+DistNode::~DistNode() {
     
 }
 
-Distribution::Distribution(int maxNodes) {    
-    this->nodes = new Node*[maxNodes];
+Distribution::Distribution(int maxNodes, int seed) {    
+    this->nodes = new DistNode*[maxNodes];
     
     for (int i = 0; i < maxNodes; i++) {
-        this->nodes[i] = new Node(-1, 0);
+        this->nodes[i] = new DistNode(-1, 0, 0);
     }
     
     this->maxNodes = maxNodes;
     this->numNodes = 0;
+    
+    this->rng = new std::mt19937(seed);
 }
 
 Distribution::~Distribution() {
@@ -34,6 +40,13 @@ Distribution::~Distribution() {
 void Distribution::addNode(double state, int count) {
     this->nodes[this->numNodes]->state = state;
     this->nodes[this->numNodes]->count = count;
+    
+    if (this->numNodes == 0) {
+        this->nodes[this->numNodes]->cumCount = count;
+    } else {
+        this->nodes[this->numNodes]->cumCount = count + this->nodes[this->numNodes - 1]->cumCount;
+    }
+    
     this->numNodes++;
 }
 
@@ -41,8 +54,13 @@ void Distribution::clear() {
     this->numNodes = 0;
 }
 
-void Distribution::update(double* states, int length) {
+void Distribution::update(double* statesArr, int length) {
     this->numNodes = 0;
+    
+    std::vector<double> states;
+    for (int i = 0; i < length; i++) {
+        states.push_back(statesArr[i]);
+    }
     
     std::sort(states.begin(), states.end());
     
@@ -60,7 +78,7 @@ void Distribution::update(double* states, int length) {
     }
 }
 
-int Distribution::getNumNodes const {
+int Distribution::getNumNodes() const {
     return this->numNodes;
 }
 
@@ -70,4 +88,71 @@ double Distribution::getState(int nodeId) const {
 
 int Distribution::getCount(int nodeId) const {
     return this->nodes[nodeId]->count;
+}
+
+double Distribution::calcDistance(Distribution* other) const {
+    double distance = 0;
+    
+    int i = 0;
+    int j = 0;
+    
+    int numNodes = this->numNodes;
+    int otherNumNodes = other->getNumNodes();
+    
+    double state = this->nodes[i]->state;
+    double otherState = other->getState(j);
+    
+    int count = this->nodes[i]->count;
+    int otherCount = other->getCount(j);
+    
+    while (i != numNodes || j != otherNumNodes) {        
+        if (state < otherState) {
+            distance += count * count;
+            i++;            
+            
+            state = this->nodes[i]->state;
+            count = this->nodes[i]->count;
+        } else if (state > otherState) {
+            distance += otherCount * otherCount;
+            j++;
+            
+            otherState = other->getState(j);
+            otherCount = other->getCount(j);
+        } else {
+            distance += (count - otherCount) * (count - otherCount);
+            i++;
+            j++;
+            
+            state = this->nodes[i]->state;
+            count = this->nodes[i]->count;
+            
+            otherState = other->getState(j);
+            otherCount = other->getCount(j);
+        }
+    }
+    
+    for (int k = i; k < numNodes; k++) {
+        count = this->nodes[k]->count;
+        distance += count * count;
+    }
+    
+    for (int k = j; k < otherNumNodes; k++) {        
+        otherCount = other->getCount(k);
+        distance += otherCount * otherCount;
+    }
+    
+    return sqrt((double) distance);
+}
+
+double Distribution::sample() const {
+    double count = 1.0 * (*this->rng)() / this->rng->max() * this->maxNodes;
+    
+    int i;
+    for (i = 0; i < this->numNodes; i++) {
+        if (count < this->nodes[i]->cumCount) {
+            break;
+        }
+    }
+    
+    return this->nodes[i]->state;
 }
