3c6be98754342e60ffb02d1fa1de8f98c0e373ce
noahtreb <noahtreb@b9da0bf2-66db-0f8a-60db-2360e31b104e>
2014-01-28 07:10:28 +0000

    Added simulation timing elements.

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -142,16 +142,21 @@ int main(int argc, const char* argv[]) {
     omp_init_lock(&lock);
         
     System* sys;
-    double initStart[numTrials];
-    double initEnd[numTrials];
-    double writeStart[numTrials];
-    double writeEnd[numTrials];
-    double revWriteStart[numTrials];
-    double revWriteEnd[numTrials];
-    double fwdWriteStart[numTrials];
-    double fwdWriteEnd[numTrials];
     
-    double progStart;
+    if (skipRefine) {
+        numDataSavePts = 0;
+    }
+    
+    double fwdSetupStart[numDataSavePts + 1];
+    double fwdSetupEnd[numDataSavePts + 1];
+    double fwdTrialStart[numDataSavePts + 1][numTrials];
+    double fwdWriteStart[numDataSavePts + 1][numTrials];
+    double fwdTrialEnd[numDataSavePts + 1][numTrials];
+    double revSetupStart[numDataSavePts + 1];
+    double revSetupEnd[numDataSavePts + 1];
+    double revTrialStart[numDataSavePts + 1][numTrials];
+    double revWriteStart[numDataSavePts + 1][numTrials];
+    double revTrialEnd[numDataSavePts + 1][numTrials];
     
     int j = 0;
     int* distSpeciesKey = new int[numBoundedSpeciesStates];
@@ -185,31 +190,45 @@ int main(int argc, const char* argv[]) {
     for (int i = 0; i < numBoundedSpeciesStates; i++) {
         fwdDists[i] = new Distribution(masterSys->species[distSpeciesKey[i]], numTrials, rng());
     }
-    
-    if (!skipInitFwd) {                
-        progStart = omp_get_wtime();
-        
+       
+    double progStart = omp_get_wtime();
+    if (!skipInitFwd) {        
+        fwdSetupStart[0] = omp_get_wtime();
         for (int i = 0; i < numBoundedSpeciesStates; i++) {
             revDists[i]->addNode(revDists[i]->species->state, numTrials);
         }
+        fwdSetupEnd[0] = omp_get_wtime();
         
         #pragma omp parallel for default(shared) private(sys)
-        for (int i = 0; i < numTrials; i++) {        
+        for (int i = 0; i < numTrials; i++) {
+            fwdTrialStart[0][i] = omp_get_wtime();
             int threadId = omp_get_thread_num();            
-            
-            initStart[i] = omp_get_wtime();        
+                    
             sys = new System(*masterSys);
             sys->seed(rng());
             sys->initFwd();
-            initEnd[i] = omp_get_wtime();
             
             varName = "initFwdData";
             simFwd(sys, numTimePts, time, state[threadId], revDists, speciesDistKey);
+            
+            fwdWriteStart[0][i] = omp_get_wtime();
             writeStateData(-1, sys, fi, varName, i, state[threadId], numTimePts, lock);
+            fwdTrialEnd[0][i] = omp_get_wtime();
+        }
+    } else {
+        fwdSetupStart[0] = 0;
+        fwdSetupEnd[0] = 0;
+        
+        for (int i = 0; i < numTrials; i++) {
+            fwdTrialStart[0][i] = 0;
+            fwdWriteStart[0][i] = 0;
+            fwdTrialEnd[0][i] = 0;
         }
     }
     
     if (!skipInitRev) {
+        revSetupStart[0] = omp_get_wtime();
+        
         masterSys->rxnPq->minHeap = false;
         masterSys->time = time[numTimePts - 1];
         lastFwdStatePt = fi->readInitDataPt("initFwdData", numTrials, masterSys->numSpecies, numTimePts - 1, numTimePts);
@@ -217,9 +236,12 @@ int main(int argc, const char* argv[]) {
         for (int i = 0; i < numBoundedSpeciesStates; i++) {             
             fwdDists[i]->update(lastFwdStatePt, numTrials);
         }
+                
+        revSetupEnd[0] = omp_get_wtime();
         
         #pragma omp parallel for default(shared) private(sys)
         for (int i = 0; i < numTrials; i++) {    
+            revTrialStart[0][i] = omp_get_wtime();
             int threadId = omp_get_thread_num(); 
             
             sys = new System(*masterSys);
@@ -237,12 +259,26 @@ int main(int argc, const char* argv[]) {
 
             varName = "initRevData";
             simRev(sys, numTimePts, time, state[threadId], fwdDists, speciesDistKey);
+            
+            revWriteStart[0][i] = omp_get_wtime();
             writeStateData(-1, sys, fi, varName, i, state[threadId], numTimePts, lock);
+            revTrialEnd[0][i] = omp_get_wtime();
+        }
+    } else {
+        revSetupStart[0] = 0;
+        revSetupEnd[0] = 0;
+        
+        for (int i = 0; i < numTrials; i++) {
+            revTrialStart[0][i] = 0;
+            revWriteStart[0][i] = 0;
+            revTrialEnd[0][i] = 0;
         }
     }  
     
     if (!skipRefine) {        
         for (int i = 0; i < numDataSavePts; i++) {
+            fwdSetupStart[i + 1] = omp_get_wtime();
+            
             masterSys->rxnPq->minHeap = true;
             masterSys->time = time[0];
             if (i == 0) {
@@ -255,8 +291,12 @@ int main(int argc, const char* argv[]) {
                 revDists[i]->update(lastRevStatePt, numTrials);
             }
             
+            fwdSetupEnd[i + 1] = omp_get_wtime();
+            
             #pragma omp parallel for default(shared) private(sys)
             for (int j = 0; j < numTrials; j++) {                
+                fwdTrialStart[i + 1][j] = omp_get_wtime();
+                
                 int threadId = omp_get_thread_num(); 
 
                 sys = new System(*masterSys);
@@ -274,8 +314,13 @@ int main(int argc, const char* argv[]) {
 
                 varName = "fwdData";
                 simFwd(sys, numTimePts, time, state[threadId], revDists, speciesDistKey);
+                
+                fwdWriteStart[i + 1][j] = omp_get_wtime();
                 writeStateData(i, sys, fi, varName, j, state[threadId], numTimePts, lock);
+                fwdTrialEnd[i + 1][j] = omp_get_wtime();
             }
+            
+            revSetupStart[i + 1] = omp_get_wtime();
             
             masterSys->rxnPq->minHeap = false;
             masterSys->time = time[numTimePts - 1];
@@ -285,8 +330,11 @@ int main(int argc, const char* argv[]) {
                 fwdDists[i]->update(lastFwdStatePt, numTrials);
             }
             
+            revSetupEnd[i + 1] = omp_get_wtime();
+            
             #pragma omp parallel for default(shared) private(sys)
-            for (int j = 0; j < numTrials; j++) {                
+            for (int j = 0; j < numTrials; j++) { 
+                revTrialStart[i + 1][j] = omp_get_wtime();                
                 int threadId = omp_get_thread_num(); 
 
                 sys = new System(*masterSys);
@@ -304,80 +352,75 @@ int main(int argc, const char* argv[]) {
 
                 varName = "revData";
                 simRev(sys, numTimePts, time, state[threadId], revDists, speciesDistKey);
+                
+                revWriteStart[i + 1][j] = omp_get_wtime();
                 writeStateData(i, sys, fi, varName, j, state[threadId], numTimePts, lock);
+                revTrialEnd[i + 1][j] = omp_get_wtime();
             }
         }
     }
     
-    abort();
-    
     double progEnd = omp_get_wtime();
-    
-    double avInitTime = 0;
-    double avExecTime = 0;
-    double avWriteTime = 0;
-    double avEwTime = 0;
-    double avRevExecTime = 0;
-    double avRevWriteTime = 0;
-    double avRevEwTime = 0;
-    double avFwdExecTime = 0;
+        
+    double avFwdSetupTime = 0;
+    double avFwdTrialTime = 0;
+    double avFwdRunTime = 0;
     double avFwdWriteTime = 0;
-    double avFwdEwTime = 0;
-    double avTrialTime = 0;
     
-    for (int i = 0; i < numTrials; i++) {
-        avInitTime += initEnd[i] - initStart[i];
-        
-        avExecTime += writeStart[i] - initEnd[i];
-        avWriteTime += writeEnd[i] - writeStart[i];
-        avEwTime += writeEnd[i] - initEnd[i];
-
-        avRevExecTime += revWriteStart[i] - writeEnd[i];
-        avRevWriteTime += revWriteEnd[i] - revWriteStart[i];
-        avRevEwTime += revWriteEnd[i] - writeEnd[i];
-        
-        avFwdExecTime += fwdWriteStart[i] - revWriteEnd[i];
-        avFwdWriteTime += fwdWriteEnd[i] - fwdWriteStart[i];
-        avFwdEwTime += fwdWriteEnd[i] - revWriteEnd[i];
-        
-        avTrialTime += fwdWriteEnd[i] - initStart[i];
+    double avRevSetupTime = 0;
+    double avRevTrialTime = 0;
+    double avRevRunTime = 0;
+    double avRevWriteTime = 0;
+    
+    for (int i = 0; i < numDataSavePts; i++) {
+        avFwdSetupTime += fwdSetupEnd[i] - fwdSetupStart[i];
+        avRevSetupTime += revSetupEnd[i] - revSetupStart[i];
+            
+        for (int j = 0; j < numTrials; j++) {
+            avFwdTrialTime += fwdTrialEnd[i][j] - fwdTrialStart[i][j];
+            avFwdRunTime += fwdWriteStart[i][j] - fwdTrialStart[i][j];
+            avFwdWriteTime += fwdTrialEnd[i][j] - fwdWriteStart[i][j];
+            
+            avRevTrialTime += revTrialEnd[i][j] - revTrialStart[i][j];
+            avRevRunTime += revWriteStart[i][j] - revTrialStart[i][j];
+            avRevWriteTime += revTrialEnd[i][j] - revWriteStart[i][j];
+        }
     }
-    avInitTime /= numTrials;
-    avExecTime /= numTrials;
-    avWriteTime /= numTrials;
-    avEwTime /= numTrials;
-    avRevExecTime /= numTrials;
-    avRevWriteTime /= numTrials;
-    avRevEwTime /= numTrials;
-    avFwdExecTime /= numTrials;
-    avFwdWriteTime /= numTrials;
-    avFwdEwTime /= numTrials;
-    avTrialTime /= numTrials;
     
-    fprintf(stdout, "\nAverage system initialization time:    %e s\n\n", avInitTime);
+    avFwdSetupTime /= numDataSavePts;
+    avFwdTrialTime /= (numDataSavePts * numTrials);
+    avFwdRunTime /= (numDataSavePts * numTrials);
+    avFwdWriteTime /= (numDataSavePts * numTrials);
     
-    fprintf(stdout, "First forward simulation:\n");
-    fprintf(stdout, "    Average simulation execution time: %e s\n", avExecTime);
-    fprintf(stdout, "    Average file write execution time: %e s\n", avWriteTime);
-    fprintf(stdout, "    Average execution time:            %e s\n\n", avEwTime);
+    avRevSetupTime /= numDataSavePts;
+    avRevTrialTime /= (numDataSavePts * numTrials);
+    avRevRunTime /= (numDataSavePts * numTrials);
+    avRevWriteTime /= (numDataSavePts * numTrials);
+        
+    double avSetupTime = (avFwdSetupTime + avRevSetupTime) / 2;
+    double avTrialTime = (avFwdTrialTime + avRevTrialTime) / 2;
+    double avRunTime = (avFwdRunTime + avRevRunTime) / 2;
+    double avWriteTime = (avFwdWriteTime + avRevWriteTime) / 2;
     
-    fprintf(stdout, "First reverse simulation:\n");
-    fprintf(stdout, "    Average simulation execution time: %e s\n", avRevExecTime);
-    fprintf(stdout, "    Average file write execution time: %e s\n", avRevWriteTime);
-    fprintf(stdout, "    Average execution time:            %e s\n\n", avRevEwTime);
+    fprintf(stdout, "\nForward simulations:\n");
+    fprintf(stdout, "    Average setup time:           %e s\n", avFwdSetupTime);
+    fprintf(stdout, "    Average simulation run time:  %e s\n", avFwdRunTime);
+    fprintf(stdout, "    Average file write time:      %e s\n", avFwdWriteTime);
+    fprintf(stdout, "    Average trial execution time: %e s\n\n", avFwdTrialTime);
     
-    fprintf(stdout, "Second forward simulation:\n");
-    fprintf(stdout, "    Average simulation execution time: %e s\n", avFwdExecTime);
-    fprintf(stdout, "    Average file write execution time: %e s\n", avFwdWriteTime);
-    fprintf(stdout, "    Average execution time:            %e s\n\n", avFwdEwTime);
+    fprintf(stdout, "Reverse simulations:\n");
+    fprintf(stdout, "    Average setup time:           %e s\n", avRevSetupTime);
+    fprintf(stdout, "    Average simulation run time:  %e s\n", avRevRunTime);
+    fprintf(stdout, "    Average file write time:      %e s\n", avRevWriteTime);
+    fprintf(stdout, "    Average trial execution time: %e s\n\n", avRevTrialTime);
+    
+    fprintf(stdout, "Forward and reverse simulations:\n");
+    fprintf(stdout, "    Average setup time:           %e s\n", avSetupTime);
+    fprintf(stdout, "    Average simulation run time:  %e s\n", avRunTime);
+    fprintf(stdout, "    Average file write time:      %e s\n", avWriteTime);
+    fprintf(stdout, "    Average trial execution time: %e s\n\n", avTrialTime);
     
     fprintf(stdout, "Total program execution time:          %e s\n\n", progEnd - progStart);
-        
-    if (skipInitRev) {
-        delete[] lastRevStatePt;
-    } else if (skipInitFwd) {
-        delete[] lastFwdStatePt;
-    }
     
     delete masterSys;    
     delete fi;  
